92b9fb38e666014d458706dea9aeb8f5ddc06b57
Ankitrp123 <Ankit.Pipare@payrollcloudcorp.com>
2024-06-17 14:22:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,6 +14,6 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'ankitpcl766@gmail.com' # ur mail id
-app.config['MAIL_PASSWORD'] = 'gzwh llii xvoz fhzc' # password
+app.config['MAIL_PASSWORD'] = 'hvaxctydc' # password
 
 mail = Mail(app)
